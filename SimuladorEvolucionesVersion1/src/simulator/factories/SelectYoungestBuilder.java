6ac6aa7e64c6a8c5e2b2bceadddce708d0966f9b
azuri <azuri@AlbertoZurita>
2024-02-15 10:50:02 +0100

    factory en proceso

---
+++ SimuladorEvolucionesVersion1/src/simulator/factories/SelectYoungestBuilder.java
@@ -0,0 +1,20 @@
+package simulator.factories;
+
+import org.json.JSONObject;
+
+public class SelectYoungestBuilder extends Builder<Object> {
+
+	public SelectYoungestBuilder(String type_tag, String desc) {
+		super(type_tag, desc);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Object create_instance(JSONObject data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
